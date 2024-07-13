4e8c4134492651d16d633d0e222770926d3a5481
mbharti321 <mbharti321@gmail.com>
2024-07-14 03:36:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <center>
+          <p>Hello World!</p>
+          <h3>Welcome to Business listing website!</h3>
+        </center>
+      </header>
+    </div>
+  );
+}
+
+export default App;
